37af5332d2255f4d753e69ba7eba1c6f3ba7ae69
Vallaser <Vallaser@outlook.com>
2020-11-26 18:54:45 +0100

    Angular

--- BACK/bugTracking/src/main/java/qc/fr/uphf/bugTracking/repositories/BugRepository.java
+++ BACK/bugTracking/src/main/java/qc/fr/uphf/bugTracking/repositories/BugRepository.java
@@ -1,11 +1,11 @@
-/*package qc.fr.uphf.bugTracking.repositories;
+package qc.fr.uphf.bugTracking.repositories;
+
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import qc.fr.uphf.bugTracking.entities.Bug;
 
-@Repository
 public interface BugRepository extends JpaRepository<Bug, Integer>{
-	Bug findById(int id);
-}*/
+	Optional<Bug> findById(Integer id);
+}
