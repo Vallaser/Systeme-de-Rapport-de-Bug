199185c4cea3ba04781d59fd57c30f5d00972a1e
Aurelien Pietrzak <pietrzak.aurelien@gmail.com>
2020-11-29 12:50:49 +0100

    Correction data.sql

--- BACK/bugTracking/src/main/resources/data.sql
+++ BACK/bugTracking/src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO Bug(id, title, description, priority, etat, dateCreation) VALUES
-  (1, 'titrebug', 'descbug', 'MOYENNE', 'TODO', '21-11-2020');
+INSERT INTO Bug(id, title, description, priority, etat, date_creation) VALUES
+  (1, 'titrebug', 'descbug', 'MOYENNE', 'TODO', '2020-11-10');
